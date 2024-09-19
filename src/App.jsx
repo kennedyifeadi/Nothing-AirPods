88e4694f4ac0d140deb8a33440beaa65fd02b17a
kennedyifeadi <kennedyifeadi@gmail.com>
2024-09-19 05:03:27 +0100

    Add transition and visibility properties to initial load screen
    Add useEffect hook to delay mounting of initial load screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-
+import { useState, useEffect } from 'react'
 import './App.css'
 import airpodImage from './assets/images/nothingAirpod3.png'
 
 function App() {
+  const [ismounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsMounted(true)}, 4000)
+  }, [])
 
   return (
     <div className="body">
-      <div className="initailLoadScreen">
+      <div className={`initailLoadScreen ${ismounted ? "initailLoadScreenOutOfView" : ""}`}>
         <h1>NOTHING</h1>
       </div>
       <div className="main">
-        <div className='testing'>
+        <div className="testing">
           <img src={airpodImage} alt="" />
         </div>
       </div>
