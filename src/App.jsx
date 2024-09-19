55bb3d94a0b243a0c1e5a0894738b675df95e69b
kennedyifeadi <kennedyifeadi@gmail.com>
2024-09-19 04:43:54 +0100

    Add font and styling to App.css, update App.jsx with initial loading screen and image display

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+
 import './App.css'
+import airpodImage from './assets/images/nothingAirpod3.png'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="body">
+      <div className="initailLoadScreen">
+        <h1>NOTHING</h1>
+      </div>
+      <div className="main">
+        <div className='testing'>
+          <img src={airpodImage} alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
